e516fb2abdbfe6cba370a7b573c008799d2ffc19
Ionica Bizau <bizauionica@yahoo.com>
2013-07-31 16:09:47 +0300

    Added hashcange handler.

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
+    // TODO is ready required?
 
+    $(window).on('hashchange', function () {
+        var newPage = location.hash.substring(1);
+        // TODO
+    });
 });
