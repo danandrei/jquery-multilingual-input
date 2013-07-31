82f6f7ac8f9d79bdffe3cb0afb8da3611c4d9715
Ionica Bizau <bizauionica@yahoo.com>
2013-07-31 16:25:56 +0300

    Add js navigation.

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,25 @@
 $(document).ready(function () {
-    // TODO is ready required?
+
+    $("[data-page]").addClass("hide");
+    showPage(location.hash);
 
     $(window).on('hashchange', function () {
-        var newPage = location.hash.substring(1);
-        // TODO
+        var newPage = location.hash;
+        showPage(newPage);
     });
 });
+
+function showPage (hash) {
+
+    var $menuItem = $("[href='" + hash + "']");
+    var page = hash.substring(1);
+
+    var $nav = $(".nav");
+    $nav.find("li").removeClass("active");
+    $menuItem.parent().addClass("active");
+
+    var $pages = $(".pages");
+    // TODO Fadein, fadeout
+    $pages.find("[data-page]").removeClass("active");
+    $pages.find("[data-page='" + page + "']").addClass("active");
+}
